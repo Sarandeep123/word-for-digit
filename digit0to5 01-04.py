ce96f80bf63e3fbd311ce2ae4dbca0e7c86e8d5f
Sarandeep123 <102315666+Sarandeep123@users.noreply.github.com>
2022-04-03 10:05:28 +0530

    Add files via upload

---
+++ digit0to5 01-04.py
@@ -0,0 +1,15 @@
+n=int(input("Enter any digit:"))
+if n==0:
+    print(n,"-->zero")
+elif n==1:
+    print(n,"-->one")
+elif n==2:
+    print(n,"-->two")
+elif n==3:
+    print(n,"-->three")
+elif n==4:
+    print(n,"-->four")
+elif n==5:
+    print(n,"-->five")
+else:
+    print("Invalid digit")
